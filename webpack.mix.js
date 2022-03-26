7a37a3e0592236f4a4bb84a79437cdd593c0857e
Ismayil Mirzayev <smylmrz@gmail.com>
2022-03-26 20:54:35 +0400

    project crud finished

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
+    .js("resources/js/admin.js", "public/js")
     .vue()
     .sass("resources/scss/app/app.scss", "public/css/app")
     .sass("resources/scss/admin/app.scss", "public/css/admin");
